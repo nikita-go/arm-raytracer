51411164684cd79735018c19b04bec6ea522c937
Nikita Gorshkov <gorshkovnikita@gmail.com>
2025-07-12 22:53:12 +0800

    Precompute inverse

--- include/utils/ray.h
+++ include/utils/ray.h
@@ -5,10 +5,12 @@
 
 // Ray
 struct Ray {
-    Vec3 o, d;
+    Vec3 o, d, inv_d;
 
     Ray() = default;
-    Ray(const Vec3& oo, const Vec3& dd) : o(oo), d(dd.norm()) {}
+    Ray(const Vec3& oo, const Vec3& dd) : o(oo), d(dd.norm()) {
+        inv_d = Vec3(1.0f / d.x, 1.0f / d.y, 1.0f / d.z);
+    }
 };
 
 #endif // RAY_H
