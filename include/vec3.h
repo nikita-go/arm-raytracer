af4054b7960a8068b389bc2a79e862b7a2ccf113
Nikita Gorshkov <gorshkovnikita@gmail.com>
2025-07-12 11:50:08 +0800

    Initialize

---
+++ include/vec3.h
@@ -0,0 +1,12 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+// NEON-accelerated Vec3
+struct alignas(16) Vec3 {
+    float x, y, z, w = 0;  // Padding for NEON 128-bit
+
+    Vec3() : x(0), y(0), z(0) {}
+    Vec3(float xx, float yy, float zz) : x(xx), y(yy), z(zz) {}
+};
+
+#endif // VEC3_H
