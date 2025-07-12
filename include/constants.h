9edb3468dc10134268b4c198d33cb8765b78f3f2
Nikita Gorshkov <gorshkovnikita@gmail.com>
2025-07-12 11:40:50 +0800

    Add constants

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr int WIDTH = 1920;  // 1080p
+constexpr int HEIGHT = 1080;
+constexpr int SAMPLES_PER_PIXEL = 1000;
+constexpr int MAX_DEPTH = 50;
+constexpr float INF = 1e30f;
+constexpr float EPS = 1e-4f;
+constexpr float ROULETTE = 0.95f;
+
+#endif // CONSTANTS_H
